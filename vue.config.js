dcd3c31fbc0a3497e735acde4e9a32ad09a9fc95
lchan <leochan007@163.com>
2019-03-26 20:57:56 +0800

    update depricated keyword.

--- vue.config.js
+++ vue.config.js
@@ -68,7 +68,7 @@ const build_str = 'building for production' + (process.env.TESTNET != undefined 
   process.env.TESTNET !== "" ? '(' + process.env.TESTNET + ')' : '') + '...'
 
 module.exports = {
-  baseUrl: './', //vueConf.baseUrl, // 根域上下文目录
+  publicPath: './', //vueConf.baseUrl, // 根域上下文目录
   outputDir: 'dist', // 构建输出目录
   assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
   lintOnSave: true, // 是否开启eslint保存检测，有效值：ture | false | 'error'
