389b18d0ac0818fb9cf683ae6fe98b7344f254da
lchan <leochan007@163.com>
2019-04-12 17:06:45 +0800

    fix ui issue

--- src/components/rewards/infoDlg.vue
+++ src/components/rewards/infoDlg.vue
@@ -1,37 +1,39 @@
 <template>
   <div name="reward_info">
-    <v-card :key="selectedId" class="pt-4 mx-auto" flat max-width="600">
-          <v-card-text>
-            <h3
-              class="headline mb-2"
-            >{{ $t("message.issueNumber_prefix") }}{{ rrInfo.issueNumber }}{{ $t("message.issueNumber_suffix") }}</h3>
-            <div class="red--text mb-2 subheading font-weight-bold">
-              <img v-if="rrInfo.ranking === 1" src="@/assets/gold.png" alt="avatar">
-              <img v-else-if="rrInfo.ranking === 2" src="@/assets/silver.png" alt="avatar">
-              <img v-else src="@/assets/bronze.png" alt="avatar">
-              <div class="yellow--text" v-if="rrInfo.ranking === 1">{{ $t("message.ranking1") }}</div>
-              <div class="grey--text" v-else-if="rrInfo.ranking === 2">{{ $t("message.ranking2") }}</div>
-              <div class="brown--text" v-else>{{ $t("message.ranking3") }}</div>
-            </div>
-            <v-divider></v-divider>
-            <v-layout text-xs-left wrap>
-              <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>{{ $t("message.tbl_name") }}:</v-flex>
-              <v-flex class="blue--text">{{ rrInfo.name }}</v-flex>
-            </v-layout>
-          </v-card-text>
-          <v-layout text-xs-left wrap>
-            <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>{{ $t("message.tbl_account") }}:</v-flex>
-            <v-flex>{{ rrInfo.account }}</v-flex>
-            <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>{{ $t("message.releaseDate") }}:</v-flex>
-            <v-flex>{{ transDate(rrInfo.recordTime) }}</v-flex>
-          </v-layout>
-          <v-layout text-xs-left wrap>
-            <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>{{ $t("message.tbl_tx_id") }}:</v-flex>
-            <v-flex v-if="isBlank(rrInfo.txId)">{{ $t("message.empty_hash") }}</v-flex>
-            <v-flex v-else>
-              <a :href="baseUrl + rrInfo.txId">{{ rrInfo.txId }}</a>
-            </v-flex>
-          </v-layout>
+    <v-card :key="selectedId" class="pt-4 mx-auto" flat>
+      <v-card-text>
+        <h3
+          class="headline mb-2 text-lg-center"
+        >{{ $t("message.issueNumber_prefix") }}{{ rrInfo.issueNumber }}{{ $t("message.issueNumber_suffix") }}</h3>
+        <div class="red--text mb-2 subheading font-weight-bold text-lg-center">
+          <img v-if="rrInfo.ranking === 1" src="@/assets/gold.png" alt="avatar">
+          <img v-else-if="rrInfo.ranking === 2" src="@/assets/silver.png" alt="avatar">
+          <img v-else src="@/assets/bronze.png" alt="avatar">
+          <div class="yellow--text" v-if="rrInfo.ranking === 1">{{ $t("message.ranking1") }}</div>
+          <div class="grey--text" v-else-if="rrInfo.ranking === 2">{{ $t("message.ranking2") }}</div>
+          <div class="brown--text" v-else>{{ $t("message.ranking3") }}</div>
+        </div>
+        <v-divider></v-divider>
+      <v-layout text-xs-left wrap>
+        <v-flex tag="strong" xs2 text-xs-left mr-0 mb-2>{{ $t("message.tbl_name") }}:</v-flex>
+        <v-flex class="blue--text">{{ rrInfo.name }}</v-flex>
+      </v-layout>
+      <v-layout text-xs-left wrap>
+        <v-flex tag="strong" xs2 text-xs-left mr-0 mb-2>{{ $t("message.tbl_account") }}:</v-flex>
+        <v-flex>{{ rrInfo.account }}</v-flex>
+      </v-layout>
+      <v-layout text-xs-left wrap>
+        <v-flex tag="strong" xs2 text-xs-left mr-0 mb-2>{{ $t("message.releaseDate") }}:</v-flex>
+        <v-flex>{{ transDate(rrInfo.recordTime) }}</v-flex>
+      </v-layout>
+      <v-layout text-xs-left wrap>
+        <v-flex tag="strong" xs2 text-xs-left mr-0 mb-2>{{ $t("message.tbl_tx_id") }}:</v-flex>
+        <v-flex v-if="isBlank(rrInfo.txId)">{{ $t("message.empty_hash") }}</v-flex>
+        <v-flex v-else>
+          <a :href="baseUrl + rrInfo.txId">{{ rrInfo.txId }}</a>
+        </v-flex>
+      </v-layout>
+      </v-card-text>
     </v-card>
   </div>
 </template>
@@ -46,18 +48,24 @@
 </style>
 
 <script>
-import { toLocalDate } from '@/utils/dt_tools';
+import { toLocalDate } from "@/utils/dt_tools";
 
-import voca from 'voca';
+import voca from "voca";
+
+import {
+  txUrl,
+} from '@/config/app_config';
 
 export default {
   props: {
-    selectedId: '',
+    selectedId: "",
     rrInfo: {}
   },
   components: {},
   data() {
-    return {};
+    return {
+      baseUrl: txUrl,
+    };
   },
   created() {
     console.log("rrinfo:", this.rrInfo);
