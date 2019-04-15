3203dee2bc303b3c60560df5fa4dff6905419f80
lchan <leochan007@163.com>
2019-04-15 15:27:44 +0800

    update to https.

--- config/kylin-stg.env.js
+++ config/kylin-stg.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   TIMEOUT: '10000',
-  BACKEND_URL: '"http://credit-stg.alphacario.com"',
+  BACKEND_URL: '"https://credit-stg.alphacario.com"',
   ACCOUNT: '"alphacarshow"',
   ACCOUNT_URL: '"https://kylin.eosx.io/account/"',
   TX_URL: '"https://kylin.eosx.io/tx/"'
